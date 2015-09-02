e7156a2105181460fc4cdd0cea83311c4e1d6c07
Jagaa <jagaa1103@gmail.com>
2015-09-02 15:01:52 +0900

    create Project

---
+++ LocBean/LocBeanTests/LocBeanTests.swift
@@ -0,0 +1,36 @@
+//
+//  LocBeanTests.swift
+//  LocBeanTests
+//
+//  Created by Enkhjargal Gansukh on 9/2/15.
+//  Copyright (c) 2015 Enkhjargal Gansukh. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class LocBeanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
