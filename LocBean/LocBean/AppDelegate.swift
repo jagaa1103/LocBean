90e6f3e6bfcbbdd50883333d59e96174ef5ee8bf
Jagaa <jagaa1103@gmail.com>
2015-09-12 16:44:24 +0900

    Beacon service deleted

--- LocBean/LocBean/AppDelegate.swift
+++ LocBean/LocBean/AppDelegate.swift
@@ -1,4 +1,4 @@
- //
+//
 //  AppDelegate.swift
 //  LocBean
 //
@@ -7,24 +7,35 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
+    
     var window: UIWindow?
-
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: .Alert | .Badge | .Sound, categories: nil))
+    var locationManager: CLLocationManager?
+    var lastProximity: CLProximity?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        let uuidString = "01122334-4556-6778-899a-abbccddeeff0"
+        let beaconIdentifier = "ibeacon"
+        let beaconUUID:NSUUID = NSUUID(UUIDString: uuidString)!
+        let beaconRegion:CLBeaconRegion = CLBeaconRegion(proximityUUID: beaconUUID,
+            identifier: beaconIdentifier)
         
-//        var localNotification = UILocalNotification()
-//        localNotification.fireDate = NSDate(timeIntervalSinceNow: 2)
-//        localNotification.timeZone = NSTimeZone.defaultTimeZone()
-//        localNotification.applicationIconBadgeNumber = -1
-//        application.scheduleLocalNotification(localNotification)
+        locationManager = CLLocationManager()
         
-        application.applicationIconBadgeNumber = -1
+        if(locationManager!.respondsToSelector("requestAlwaysAuthorization")) {
+            locationManager!.requestAlwaysAuthorization()
+        }
+        
+        locationManager!.delegate = self
+        locationManager!.pausesLocationUpdatesAutomatically = false
+        
+        locationManager!.startMonitoringForRegion(beaconRegion)
+        locationManager!.startRangingBeaconsInRegion(beaconRegion)
+        locationManager!.startUpdatingLocation()
+        
         if(application.respondsToSelector("registerUserNotificationSettings:")) {
             application.registerUserNotificationSettings(
                 UIUserNotificationSettings(
@@ -33,32 +44,102 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 )
             )
         }
-        BeaconService.sharedInstance.startService();
+        
         return true
     }
-
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    
+}
 
-
+extension AppDelegate: CLLocationManagerDelegate {
+    func sendLocalNotificationWithMessage(message: String!) {
+        let notification:UILocalNotification = UILocalNotification()
+        notification.alertBody = message
+        notification.soundName = "tos_beep.caf";
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    }
+    
+    func locationManager(manager: CLLocationManager!,
+        didRangeBeacons beacons: [AnyObject]!,
+        inRegion region: CLBeaconRegion!) {
+            let viewController:ViewController = window!.rootViewController as! ViewController
+            viewController.beacons = beacons as! [CLBeacon]?
+            viewController.tableView!.reloadData()
+            
+            NSLog("didRangeBeacons");
+            var message:String = ""
+            
+            if(beacons.count > 0) {
+                let nearestBeacon:CLBeacon = beacons[0] as! CLBeacon
+                
+                if(nearestBeacon.proximity == lastProximity ||
+                    nearestBeacon.proximity == CLProximity.Unknown) {
+                        return;
+                }
+                lastProximity = nearestBeacon.proximity;
+                
+                switch nearestBeacon.proximity {
+                case CLProximity.Far:
+                    message = "You are far away from the beacon"
+                case CLProximity.Near:
+                    message = "You are near the beacon"
+                case CLProximity.Immediate:
+                    message = "You are in the immediate proximity of the beacon"
+                case CLProximity.Unknown:
+                    return
+                }
+            } else {
+                
+                if(lastProximity == CLProximity.Unknown) {
+                    return;
+                }
+                
+                message = "No beacons are nearby"
+                lastProximity = CLProximity.Unknown
+            }
+            
+            NSLog("%@", message)
+            sendLocalNotificationWithMessage(message)
+    }
+    
+    func locationManager(manager: CLLocationManager!,
+        didEnterRegion region: CLRegion!) {
+            manager.startRangingBeaconsInRegion(region as! CLBeaconRegion)
+            manager.startUpdatingLocation()
+            
+            NSLog("You entered the region")
+            sendLocalNotificationWithMessage("You entered the region")
+    }
+    
+    func locationManager(manager: CLLocationManager!,
+        didExitRegion region: CLRegion!) {
+            manager.stopRangingBeaconsInRegion(region as! CLBeaconRegion)
+            manager.stopUpdatingLocation()
+            
+            NSLog("You exited the region")
+            sendLocalNotificationWithMessage("You exited the region")
+    }
 }
 
--- LocBean/LocBean/ViewController.swift
+++ LocBean/LocBean/ViewController.swift
@@ -9,31 +9,69 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet var tableView: UITableView?
+    var beacons: [CLBeacon]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        println("ViewController is inited")
         // Do any additional setup after loading the view, typically from a nib.
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     
-    @IBOutlet weak var labelProximity: UILabel!
-    @IBOutlet weak var labelMinor: UILabel!
-    @IBOutlet weak var labelMajor: UILabel!
-    @IBOutlet weak var labelRSSI: UILabel!
-    @IBOutlet weak var labelUUID: UILabel!
-    @IBAction func startClicked(sender: AnyObject) {
-        BeaconService.sharedInstance.startService()
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            if(beacons != nil) {
+                return beacons!.count
+            } else {
+                return 0
+            }
     }
-    @IBAction func stopClicked(sender: AnyObject) {
-        BeaconService.sharedInstance.stopService()
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            var cell:UITableViewCell? =
+            tableView.dequeueReusableCellWithIdentifier("MyIdentifier") as? UITableViewCell
+            
+            if(cell == nil) {
+                cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyIdentifier")
+                cell!.selectionStyle = UITableViewCellSelectionStyle.None
+            }
+            
+            let beacon:CLBeacon = beacons![indexPath.row]
+            var proximityLabel:String! = ""
+            
+            switch beacon.proximity {
+            case CLProximity.Far:
+                proximityLabel = "Far"
+            case CLProximity.Near:
+                proximityLabel = "Near"
+            case CLProximity.Immediate:
+                proximityLabel = "Immediate"
+            case CLProximity.Unknown:
+                proximityLabel = "Unknown"
+            }
+            
+            cell!.textLabel!.text = proximityLabel
+            
+            let detailLabel:String = "Major: \(beacon.major.integerValue), " +
+                "Minor: \(beacon.minor.integerValue), " +
+                "RSSI: \(beacon.rssi as Int), " +
+            "UUID: \(beacon.proximityUUID.UUIDString)"
+            cell!.detailTextLabel!.text = detailLabel
+            
+            return cell!
     }
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
